a863eac996ef480e03b06a066eb1b3a872b2c676
Dai Huynh <dai.huynh@ucdenver.edu>
2021-03-23 16:17:18 -0600

    loop through github profile success

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+// import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
 
 class App extends Component {
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <Users />
+
       </div>
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,26 +3,26 @@ import UserItem from './UserItem';
 
 class Users extends Component {
   state = {
-    user: [
+    users: [
       {
         id: 1,
         login: 'mojombo',
         avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
+        html_url: 'https://github.com/mojombo'
       },
       {
         id: 3,
         login: 'pjhyett',
         avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
+        html_url: 'https://github.com/pjhyett'
       },
       {
         login: 'ezmobius',
         id: 5,
         avatar_url: 'https://avatars.githubusercontent.com/u/5?v=4',
-        html_url: 'https://github.com/ezmobius',
-      },
-    ],
+        html_url: 'https://github.com/ezmobius'
+      }
+    ]
   };
   render() {
     return (
